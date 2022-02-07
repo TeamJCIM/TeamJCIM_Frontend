955dd2a386c77dd5db7c33953699b7cb39c61d30
yuncheol-AHN <anchul@hanmail.net>
2022-02-07 13:26:52 +0900

    Revert "MAKE overview"
    This reverts commit b26bc42b3e9a4b42ac2ee643435b207905b838fb.

--- src/components/Cards/Info/index.js
+++ src/components/Cards/Info/index.js
@@ -14,27 +14,23 @@ class CardInfo extends Component {
     }
 
     componentDidMount() {
-        this.setState({cardClass: `card border-left-${this.props.color} shadow h-60 py-4`})
-        this.setState({icon: `fas fa-${this.props.icon} fa-2x text-${this.props.color}`})
+        this.setState({cardClass: `card border-left-${this.props.color} shadow h-100 py-2`})
+        this.setState({icon: `fas fa-${this.props.icon} fa-2x text-gray-300`})
         this.setState({titleClass: `text-xs font-weight-bold text-${this.props.color} text-uppercase mb-1`})
     }
 
     render() {
         return (
             <div className="col-xl-3 col-md-6 mb-4">
-                <div className={this.state.titleClass}>{this.state.title}</div>
-                
                 <div className={this.state.cardClass}>
                     <div className="card-body">
                         <div className="row no-gutters align-items-center">
-                            
-                            <div className="col mr-2 align">
-                                <i className={this.state.icon}></i>
-                            </div>
-
-                            <div className="col-auto">
-                                {/* <div className={this.state.titleClass}>{this.props.title}</div> */}
+                            <div className="col mr-2">
+                                <div className={this.state.titleClass}>{this.props.title}</div>
                                 <div className="h5 mb-0 font-weight-bold text-gray-800">{this.state.value}</div>
+                            </div>
+                            <div className="col-auto">
+                                <i className={this.state.icon}></i>
                             </div>
                         </div>
                     </div>
