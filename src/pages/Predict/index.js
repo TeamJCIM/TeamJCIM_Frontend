ef5c2e73f7ee93277d5c48e0a948145b9d9e147e
myeongheonhong <ruddyh7076@gmail.com>
2022-03-09 12:31:20 +0900

    함수형 컴포넌트로 변경, 사용하지 않는 import 제거.

--- src/pages/Predict/index.js
+++ src/pages/Predict/index.js
@@ -1,61 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 //Navigation
 import Sidebar from '../../components/Navigation/Sidebar';
 import Topbar from '../../components/Navigation/Topbar';
-
-import CardInfo from '../../components/Cards/Info';
-import ChartDonut from '../../components/Charts/Donut';
 import ChartLine from '../../components/Charts/Line';
 import PageHeading from '../../components/PageHeading';
-import CardBasic from '../../components/Cards/Basic';
-
-import ChartBar from '../../components/Charts/Bar';
 import NextMonth from '../../components/NextMonth';
 import PredictInfo from '../../components/Cards/PredictInfo';
 
-class Predict extends Component {
-    render() {
-        return (
-            <div>
-                <div id="wrapper">
-                    <Sidebar />
-                    <div id="content-wrapper" className="=d-flex flex-column">
-                        <Topbar />
-                        <div className="container-fluid">
-                            <PageHeading title="전력예측" />
-                        </div>
-                        <div className="container-fluid">
-                            <ChartLine title="이번달 전력예측"></ChartLine>
-                        </div>
+export default function Predict() {
+    return (
+        <div>
+            <div id="wrapper">
+                <Sidebar />
+                <div id="content-wrapper" className="=d-flex flex-column">
+                    <Topbar />
+                    <div className="container-fluid">
+                        <PageHeading title="전력예측" />
+                    </div>
+                    <div className="container-fluid">
+                        <ChartLine title="이번달 전력예측"></ChartLine>
+                    </div>
 
-                        <div className="container-fluid">
-                            <div className="row">
-                                <PredictInfo
-                                    title="예상요금액"
-                                    icon="won"
-                                    color="primary"
-                                    value="15,821(원)"
-                                />
+                    <div className="container-fluid">
+                        <div className="row">
+                            <PredictInfo
+                                title="예상요금액"
+                                icon="won"
+                                color="primary"
+                                value="15,821(원)"
+                            />
 
-                                <PredictInfo
-                                    title="예상전력사용량"
-                                    icon=""
-                                    color="primary"
-                                    value="4,539(kWh)"
-                                />
-                            </div>
+                            <PredictInfo
+                                title="예상전력사용량"
+                                icon=""
+                                color="primary"
+                                value="4,539(kWh)"
+                            />
                         </div>
+                    </div>
 
-                        <div className="container-fluid">
-                            <NextMonth title="다음달 전력예측"></NextMonth>
-                            <div></div>
-                        </div>
+                    <div className="container-fluid">
+                        <NextMonth title="다음달 전력예측"></NextMonth>
+                        <div></div>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default Predict;
