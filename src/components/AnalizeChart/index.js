6dae6ace4d258250083b513cbc5a575e01d5a27f
myeongheonhong <ruddyh7076@gmail.com>
2022-04-27 17:49:03 +0900

    동기방식으로 그래프 표현.

--- src/components/AnalizeChart/index.js
+++ src/components/AnalizeChart/index.js
@@ -10,91 +10,129 @@ Chart.defaults.global.defaultFontColor = '#858796';
 export default function AnalizeChart(props) {
     const chartRef = React.createRef();
 
+    const pArray = [];
+    const iArray = [];
+
     useEffect(() => {
-        axios
-            .get(`/api/safety/PowerAnalysis/1227564000/2021`)
-            .then(function (response) {
+        const analizeChartRef = chartRef.current.getContext('2d');
+        async function fetch() {
+            const response = await axios.get(
+                `/api/safety/PowerAnalysis/1227564000/2021`
+            );
+            try {
                 response['data']['data'][0].forEach((element) => {
-                    // const idx = element['Month'] - 1;
+                    const idx = element['Month'] - 1;
+                    pArray[idx] = element['PredictData'];
                 });
                 response['data']['data'][1].forEach((element) => {
-                    // const idx = element['Month'] - 1;
+                    const idx = element['Month'] - 1;
+                    iArray[idx] = element['IotData'];
                 });
-            })
-            .catch(function (error) {
+            } catch (error) {
                 console.log(error);
+            }
+            drawGraph();
+        }
+        fetch();
+        function drawGraph() {
+            const data = {
+                labels: [
+                    '1월',
+                    '2월',
+                    '3월',
+                    '4월',
+                    '5월',
+                    '6월',
+                    '7월',
+                    '8월',
+                    '9월',
+                    '10월',
+                    '11월',
+                    '12월',
+                ],
+                datasets: [
+                    {
+                        label: '실제 전력 사용량',
+                        axis: 'y',
+                        data: iArray,
+                        fill: false,
+                        backgroundColor: [
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                        ],
+                        borderColor: [
+                            'rgb(75, 192, 192)',
+                            'rgb(75, 192, 192)',
+                            'rgb(75, 192, 192)',
+                            'rgb(75, 192, 192)',
+                            'rgb(75, 192, 192)',
+                            'rgb(75, 192, 192)',
+                            'rgb(75, 192, 192)',
+                            'rgb(75, 192, 192)',
+                            'rgb(75, 192, 192)',
+                            'rgb(75, 192, 192)',
+                            'rgb(75, 192, 192)',
+                            'rgb(75, 192, 192)',
+                        ],
+                        borderWidth: 1,
+                    },
+                    {
+                        label: '예상 전력 사용량',
+                        axis: 'y',
+                        fill: false,
+                        data: pArray,
+                        backgroundColor: [
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                        ],
+                        borderColor: [
+                            'rgb(54, 162, 235)',
+                            'rgb(54, 162, 235)',
+                            'rgb(54, 162, 235)',
+                            'rgb(54, 162, 235)',
+                            'rgb(54, 162, 235)',
+                            'rgb(54, 162, 235)',
+                            'rgb(54, 162, 235)',
+                            'rgb(54, 162, 235)',
+                            'rgb(54, 162, 235)',
+                            'rgb(54, 162, 235)',
+                            'rgb(54, 162, 235)',
+                            'rgb(54, 162, 235)',
+                        ],
+                        borderWidth: 1,
+                    },
+                ],
+            };
+            new Chart(analizeChartRef, {
+                type: 'horizontalBar',
+                data: data,
+                options: {
+                    scales: {
+                        indexAxis: 'y',
+                    },
+                },
             });
-
-        const myChartRef = chartRef.current.getContext('2d');
-        const data = {
-            labels: [
-                '1월',
-                '2월',
-                '3월',
-                '4월',
-                '5월',
-                '6월',
-                '7월',
-                '8월',
-                '9월',
-                '10월',
-                '11월',
-                '12월',
-            ],
-            datasets: [
-                {
-                    label: '실제 전력 사용량',
-                    axis: 'y',
-                    data: '',
-                    fill: false,
-                    backgroundColor: [
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                    ],
-                    borderColor: [
-                        'rgb(75, 192, 192)',
-                        'rgb(75, 192, 192)',
-                        'rgb(75, 192, 192)',
-                        'rgb(75, 192, 192)',
-                        'rgb(75, 192, 192)',
-                    ],
-                    borderWidth: 1,
-                },
-                {
-                    label: '예상 전력 사용량',
-                    axis: 'y',
-                    fill: false,
-                    data: [3908, 3029, 2015, 3928, 4239],
-                    backgroundColor: [
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                    ],
-                    borderColor: [
-                        'rgb(54, 162, 235)',
-                        'rgb(54, 162, 235)',
-                        'rgb(54, 162, 235)',
-                        'rgb(54, 162, 235)',
-                        'rgb(54, 162, 235)',
-                    ],
-                    borderWidth: 1,
-                },
-            ],
-        };
-        new Chart(myChartRef, {
-            type: 'horizontalBar',
-            data: data,
-            options: {
-                scales: {
-                    indexAxis: 'y',
-                },
-            },
-        });
+        }
     });
 
     return (
