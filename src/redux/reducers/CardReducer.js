ba6c778a80d219f5353eb4e1fc31e6fe645b8755
안윤철 <anchul@hanmail.net>
2022-05-18 17:02:19 +0900

    2차

--- src/redux/reducers/CardReducer.js
+++ src/redux/reducers/CardReducer.js
@@ -1,7 +1,7 @@
 import { CARD_DATA } from "../actions/ActionTypes";
 
 const initialState = {
-    iotNum: false
+    
 }
 
 export const CardReducer = (state = initialState, action) => {
@@ -9,8 +9,40 @@ export const CardReducer = (state = initialState, action) => {
     console.log('card reducer', action)
     switch (action.type) {
         case CARD_DATA:
+            let iotData = action.data[0][0].IotData
+            let predictData = action.data[1]
+            let iotState = ""
+            let electricData = []
+
+            if (action.data[2].AlarmVoltage === 4
+                || action.data[2].AlarmElectric === 2
+                || action.data[2].AlarmLeakage === 2
+                || action.data[2].AlarmArc === 1
+                || action.data[2].AlarmTemperature === 1) {
+                iotState = 'Danger'
+            } else if (action.data[2].AlarmVoltage === 1
+                || action.data[2].AlarmVoltage === 2
+                || action.data[2].AlarmElectric === 1
+                || action.data[2].AlarmLeakage === 1) {
+                iotState ='Warning'
+            } else {
+                iotState = 'Success'
+            }
+
+            for (let i = 0; i < action.data[3].length; i++) {
+                electricData[Math.floor(Number(action.data[3][i].Date.substr(11, 2)) / 2)] += Math.floor(action.data[3][i].VoltageAvg)
+            }
+
+            for (let i = 0; i < action.data[3].length; i++) {
+                electricData[Math.floor(Number(action.data[3][i].Date.substr(11, 2)))] += Math.floor(action.data[3][i].VoltageAvg)
+            }
+
             return {
-                
+                ...state,
+                iotData: iotData,
+                predictData: predictData,
+                iotState: iotState,
+                electricData: electricData,
             };
         default:
             return state;
